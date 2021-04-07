3dba257f9d027712fb364c05dd13b05d282729e5
DanDoDev <dangudevdev@gmail.com>
2021-04-07 02:03:19 -0400

    img rotate success

--- 8.rotate-img/index.js
+++ 8.rotate-img/index.js
@@ -13,15 +13,27 @@
         matrix[1][0] = memory2;
         matrix[0][0] = memory;
         return matrix;  
-    };
+    }
+
     const gridLength = matrix.length;
     let currentGridLength = gridLength;
     
     for(let i=0; i < gridLength - 2; i++){
         // i acts as increment for inner squares;
-    console.log('grid iter', currentGridLength - (2 * i))
-        currentGridLength = currentGridLength - (2 * i);
-        if(currentGridLength < 2)break;
+
+        currentGridLength -= i > 0 ? 2 : 0;
+
+        if(currentGridLength === 2){
+            let memory = matrix[0 + i][1 + i];
+            matrix[0 + i][1 + i] = matrix[0 + i][0 + i];
+            let memory2 = matrix[1 + i][1 + i];
+            matrix[1 + i][1 + i] = memory;
+            memory = matrix[1 + i][0 + i];
+            matrix[1 + i][0 + i] = memory2;
+            matrix[0 + i][0 + i] = memory;
+            break;
+        }else if(currentGridLength < 2)break;
+
         for(let j=0; j < currentGridLength - 1; j++){
             // j keeps track of moves necessary to finish a ring 
             let positionInMemory = null;
@@ -31,32 +43,25 @@
                 switch(k){
                     case 0: 
                         positionInMemory = matrix[0 + i + j][gridLength - 1 - i];
-                        console.log('move1', positionInMemory, matrix[0 + i][0 + i + j]);
                         matrix[0 + i + j][gridLength - 1 - i] = matrix[0 + i][0 + i + j];
                         break;
                     case 1:
                         secondPositionInMemory = matrix[gridLength - 1 - i][gridLength - 1 - i - j];
-                        console.log('move2', secondPositionInMemory, positionInMemory);
                         matrix[gridLength - 1 - i][gridLength - 1 - i - j] = positionInMemory;
                         break;
                     case 2:
                         positionInMemory = matrix[gridLength - 1 - i - j][0 + i];
-                        console.log('move3', positionInMemory, secondPositionInMemory);
                         matrix[gridLength - 1 - i - j][0 + i] = secondPositionInMemory;
                         break;
                     case 3:
-                        console.log('move4', matrix[0 + i][0 + i + j], positionInMemory);
                         matrix[0 + i][0 + i + j] = positionInMemory;
                         break;
                 }
             }
         }
     }
-    
-    console.log(matrix)
+
     return matrix;
-    
-    
 };
 
 
