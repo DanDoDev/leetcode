be05c12e0a5e974cbd231687da997b302661995f
DanDoDev <dangudevdev@gmail.com>
2021-04-07 01:16:47 -0400

    solves 3x3

--- 8.rotate-img/index.js
+++ 8.rotate-img/index.js
@@ -9,7 +9,7 @@
     
     for(let i=0; i < gridLength - 2; i++){
         // i acts as increment for inner squares;
-        for(let j=0; j < gridLength -2; j++){
+        for(let j=0; j < gridLength - 1; j++){
             // j keeps track of moves necessary to finish a ring 
             let positionInMemory = null;
             let secondPositionInMemory = null;
@@ -22,9 +22,9 @@
                         matrix[0 + i + j][gridLength - 1 - i] = matrix[0 + i][0 + i + j];
                         break;
                     case 1:
-                        secondPositionInMemory = matrix[gridLength - 1 - i - j][gridLength - 1 - i - j];
+                        secondPositionInMemory = matrix[gridLength - 1 - i][gridLength - 1 - i - j];
                         console.log('move2', secondPositionInMemory, positionInMemory);
-                        matrix[gridLength - 1 - i - j][gridLength - 1 - i - j] = positionInMemory;
+                        matrix[gridLength - 1 - i][gridLength - 1 - i - j] = positionInMemory;
                         break;
                     case 2:
                         positionInMemory = matrix[gridLength - 1 - i - j][0 + i];
@@ -32,8 +32,8 @@
                         matrix[gridLength - 1 - i - j][0 + i] = secondPositionInMemory;
                         break;
                     case 3:
-                        console.log('move4', matrix[0 + i + j][0 + i + j], positionInMemory);
-                        matrix[0 + i + j][0 + i + j] = positionInMemory;
+                        console.log('move4', matrix[0 + i][0 + i + j], positionInMemory);
+                        matrix[0 + i][0 + i + j] = positionInMemory;
                         break;
                 }
             }
