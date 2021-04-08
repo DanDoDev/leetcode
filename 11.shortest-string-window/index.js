fdd189da032da8a1861cfc05a0c1aa7afa561577
DanDoDev <dangudevdev@gmail.com>
2021-04-08 01:18:22 -0400

    shortest string window rework finished and past all huge test cases

--- 11.shortest-string-window/index.js
+++ 11.shortest-string-window/index.js
@@ -3,53 +3,55 @@
  * @param {string} t
  * @return {string}
  */
- const minWindow = (s, t) => {
-    const searchingFor = {};
-    
-    let rightPointer = 0;
-    
-    let bestString = null;
-    
-    let uniqueValues = 0;
-    
-    for(let i=0;i< t.length;i++){
-        const valueNew = !searchingFor[t[i]];
-        if(valueNew)uniqueValues ++;
-        searchingFor[t[i]] = valueNew ? 1 : searchingFor[t[i]] + 1;
+const minWindow = (s, t) => {
+  const searchingFor = {};
+
+  let rightPointer = 0;
+
+  let bestString = null;
+
+  let uniqueValues = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    const valueNew = !searchingFor[t[i]];
+    if (valueNew) uniqueValues++;
+    searchingFor[t[i]] = valueNew ? 1 : searchingFor[t[i]] + 1;
+  }
+  for (let leftPointer = 0; leftPointer + t.length <= s.length; leftPointer++) {
+    const isSearchedFor = searchingFor[s[leftPointer]] !== undefined;
+
+    if (isSearchedFor && leftPointer === 0) {
+      searchingFor[s[leftPointer]]--;
+      if (searchingFor[s[leftPointer]] === 0) uniqueValues--;
     }
-    for(let leftPointer = 0; leftPointer < s.length; leftPointer++){
-        const isSearchedFor = searchingFor[s[leftPointer]] > 0;
-        
-        if(isSearchedFor){
-            searchingFor[s[leftPointer]]--;
-            if(searchingFor[s[leftPointer]] === 0)uniqueValues--;
-            console.log('left searched for', s[leftPointer], searchingFor[s[leftPointer]])
-        }
-        
-        if(leftPointer > 0){
-            const prevValueCount = searchingFor[s[leftPointer-1]];
-            console.log('prev value count', s[leftPointer-1]);
-            searchingFor[s[leftPointer -1]] = !searchingFor[s[leftPointer -1]] ? 1 : searchingFor[s[leftPointer -1]] + 1;
-            console.log('prev value count', prevValueCount === 0, leftPointer, s.length);
-            if(prevValueCount === 0)uniqueValues++;
-        }
-        
-        
-        while(uniqueValues > 0 && rightPointer < s.length){
-            rightPointer++;
-            const rightSearchedFor = searchingFor[s[rightPointer]];
-            if(rightSearchedFor){
-                console.log('rightSearchedFor', s[rightPointer])
-                searchingFor[s[rightPointer]]--;
-                if(searchingFor[s[rightPointer]] === 0)uniqueValues--;
-            }
-        }
-        if(uniqueValues === 0 && (!bestString || bestString[1] - bestString[0] > rightPointer - leftPointer)){
-            bestString = [leftPointer,rightPointer];
-        }
+
+    if (leftPointer > 0) {
+      const prevValueSearchedFor =
+        searchingFor[s[leftPointer - 1]] !== undefined;
+      if (prevValueSearchedFor) {
+        searchingFor[s[leftPointer - 1]]++;
+        if (searchingFor[s[leftPointer - 1]] === 1) uniqueValues++;
+      }
     }
-    
-    return s.substring(bestString[0], bestString[1] + 1);
-    
-    
-}
+
+    while (uniqueValues > 0 && rightPointer < s.length) {
+      rightPointer++;
+      const rightSearchedFor = searchingFor[s[rightPointer]] !== undefined;
+      if (rightSearchedFor) {
+        searchingFor[s[rightPointer]]--;
+
+        if (searchingFor[s[rightPointer]] === 0) uniqueValues--;
+      }
+    }
+
+    if (
+      uniqueValues === 0 &&
+      (!bestString ||
+        bestString[1] - bestString[0] > rightPointer - leftPointer)
+    ) {
+      bestString = [leftPointer, rightPointer];
+    } else {
+    }
+  }
+  return bestString ? s.substring(bestString[0], bestString[1] + 1) : "";
+};
