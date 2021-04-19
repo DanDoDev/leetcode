309e3aff2ca5bbe7c6f49d7e7e119bb196fa80ba
DanDoDev <dangudevdev@gmail.com>
2021-04-19 00:10:53 -0400

    4sum adjusted to use two loops + 2 pointers, now passes final performance test cases

--- 15.4sum/index.js
+++ 15.4sum/index.js
@@ -4,37 +4,44 @@
  * @return {number[][]}
  */
 const fourSum = (nums, target) => {
-    const sets = new Set();   
-    return recursiveSolution(nums, target, [0,1,2,3], 0, sets);
+  if (!Array.isArray(nums) || nums.length < 3) return [];
+
+  nums = nums.sort((a, b) => a - b);
+  const output = [];
+  let set = new Set();
+
+  for (let i = 0; i < nums.length - 3; i++) {
+    const firstVal = nums[i];
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      const secondVal = nums[j];
+
+      let leftOffset = 1;
+      let rightOffset = 0;
+      while (nums.length - 2 - j - leftOffset - rightOffset >= 0) {
+        const sum =
+          firstVal +
+          secondVal +
+          nums[j + leftOffset] +
+          nums[nums.length - 1 - rightOffset];
+
+        if (sum === target) {
+          const newArray = [
+            firstVal,
+            secondVal,
+            nums[j + leftOffset],
+            nums[nums.length - 1 - rightOffset],
+          ];
+          const setString = newArray.join(",");
+          if (!set.has(setString)) {
+            set.add(setString);
+            output.push(newArray);
+          }
+          leftOffset++;
+          rightOffset++;
+        } else if (sum < target) leftOffset++;
+        else rightOffset++;
+      }
+    }
+  }
+  return output;
 };
-
-const recursiveSolution = (nums, target, locks, lockPosition, previousSets) => {
-    if(lockPosition >= locks.length){
-        let newSet = [nums[locks[0]], nums[locks[1]], nums[locks[2]], nums[locks[3]]];
-        const sum = newSet.reduce((total,x) => total + x);
-        if(sum === target){
-            newSet = newSet.sort((a,b) => a-b);
-            const stringSet = newSet.join(',');            
-            if(!previousSets.has(stringSet)){
-                previousSets.add(stringSet);
-                return [newSet];
-            }
-        }
-        return [];
-    }
-    let newVals = [];
-    for(let i = locks[lockPosition]; i < nums.length - (3 - lockPosition);i++){
-        locks[lockPosition] = i;
-        for(let j=lockPosition + 1;j<locks.length;j++){
-            if(locks[j] <= locks[j-1]){
-                locks[j] = locks[j-1] + 1;
-            }
-        }
-        newVals = newVals.concat(recursiveSolution(nums, target, [...locks], lockPosition + 1, previousSets));
-    }
-    return newVals;
- 
-    
-}
-
-
